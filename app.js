d3c949cae184836f5d0916289a7ba591de44f85b
ashab20zakaraev <64374587+ashab20zakaraev@users.noreply.github.com>
2021-11-21 23:59:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const board = document.getElementById('board')
+const colors = ['#FFFF33', '#FF0000', '#9999CC', '#FF33FF', '#00FFCC', '#99FF33', '#6699FF', '#33FF33']
+const SQUARES_NUMBER = 750
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => {
+        setColor(square)
+    })
+
+    square.addEventListener('mouseleave', () => {
+        removeColor(square)
+    })
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.background = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.background = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
